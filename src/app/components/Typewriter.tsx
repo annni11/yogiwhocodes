efd1d75db94e3ac007af8efe9b0fb165975ea018
annni11 <ann.j.ni@gmail.com>
2024-02-17 17:50:59 -0500

    added on click

--- src/app/components/Typewriter.tsx
+++ src/app/components/Typewriter.tsx
@@ -1,38 +1,47 @@
+import Link from 'next/link';
 import { ReactTyped } from 'react-typed';
 export default function Typewriter() {
+  const handleScrollToBottom = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  };
   return (
     <div className='h-screen flex flex-col justify-evenly'>
       <div>
-        <ReactTyped strings={['Hello, World!']} typeSpeed={40}>
-          <input type='text' />
-        </ReactTyped>
+        <ReactTyped
+          strings={['Hello, World!']}
+          typeSpeed={40}
+          showCursor={false}
+        />
         <br />
         <ReactTyped
           strings={['I am Annabelle']}
           typeSpeed={40}
-          startDelay={1200}>
-          <input type='text' />
-        </ReactTyped>
+          startDelay={1200}
+          showCursor={false}
+        />
         <br />
-        <ReactTyped strings={['a.k.a.']} typeSpeed={40} startDelay={2200}>
-          <input type='text' />
-        </ReactTyped>
+        <ReactTyped
+          strings={['a.k.a.']}
+          typeSpeed={40}
+          startDelay={2200}
+          showCursor={false}
+        />
         <br />
         <ReactTyped
           strings={['yogi who codes']}
           typeSpeed={40}
-          startDelay={3000}>
-          <input type='text' />
-        </ReactTyped>
+          startDelay={3000}
+          showCursor={false}
+        />
       </div>
-      <div>
+      <div onClick={handleScrollToBottom} className='cursor-pointer'>
         <ReactTyped
           strings={['↓']}
           typeSpeed={40}
           startDelay={4800}
-          className='bold'>
-          <input type='text' />
-        </ReactTyped>
+          className='bold'
+          showCursor={false}
+        />
       </div>
     </div>
   );
