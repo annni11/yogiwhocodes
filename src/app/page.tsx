0f3304106c44c7f4429c3f966ff102f54bb044e0
annni11 <ann.j.ni@gmail.com>
2024-02-17 22:27:27 -0500

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Typewriter from './components/Typewriter';
 import Info from './components/Info';
-import FramerWrapper from './framerWrapper';
+import FramerWrapper from './FramerWrapper';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center m-2 sm:m-10'>
