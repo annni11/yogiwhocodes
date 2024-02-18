ede81528f68230b02498e3e1a22cd370f88dbfbc
annni11 <ann.j.ni@gmail.com>
2024-02-17 22:40:04 -0500

    removed framer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import Typewriter from './components/Typewriter';
 import Info from './components/Info';
-import FramerWrapper from './FramerWrapper';
+
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center m-2 sm:m-10'>
       <Typewriter />
-
-      <FramerWrapper children={<Info />} />
+      <Info />
     </main>
   );
 }
