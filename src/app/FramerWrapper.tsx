57b226faf012810d13ea791636ee7e4c6e51372f
annni11 <ann.j.ni@gmail.com>
2024-02-17 22:32:27 -0500

    lower duration

--- src/app/FramerWrapper.tsx
+++ src/app/FramerWrapper.tsx
@@ -8,7 +8,7 @@ export default function FramerWrapper({ children }) {
         <motion.div
           initial={{ opacity: 0 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 6 }}
+          transition={{ duration: 5 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}>
           {children}
